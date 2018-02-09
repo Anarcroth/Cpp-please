20a9a8fc6403fd2f50d3d4f28d5b3077bce19ef4
Martin Nestorov <mnestorov@protonmail.com>
2018-02-09 20:44:22 +0200

    Fixed indentation

--- tutorial-2/src/lambda_2.cpp
+++ tutorial-2/src/lambda_2.cpp
@@ -9,7 +9,7 @@ int main()
 	//auto y = [=]() { grade += 0.6; std::cout << "the grade is " << grade << std::endl; };
 
 	// capture local var by value, but be able to mutate it
-	auto y = [=]() mutable { grade += 0.6; std::cout << "the grade is " << grade << std::endl; };
+	auto y = [=]()  { grade += 0.6; std::cout << "the grade is " << grade << std::endl; };
 
 	y();
 
